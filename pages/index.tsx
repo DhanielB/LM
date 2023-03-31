9486395778fde2485fcab7d6a2b3afb83db1f7a3
Dhaniel Brandão <82909711+DhanielB@users.noreply.github.com>
2023-03-30 22:34:01 -0300

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,7 +17,7 @@ export default function Home() {
         <div className="left-[30rem] top-[14rem] absolute">
           <Image src={require("@/images/main.gif")} alt={""} width={500} height={500}/>
         </div><br/>
-        <button className="bg-[#5102FB] text-white px-[0.5rem] py-[0.35rem] ml-[1.5rem] mt-[4rem] rounded-md flex justify-center md:block md:justify-center">Quero mandar bem em Matemática em 2023</button>
+        <button className="bg-[#5102FB] text-white px-[0.5rem] py-[0.35rem] ml-[1.5rem] mt-[4rem] rounded-md flex justify-center md:block md:justify-normal">Quero mandar bem em Matemática em 2023</button>
         <a href="https://api.whatsapp.com/send?phone=5531995406293&text=Ol%C3%A1%2C%20estou%20no%20site%20da%20'replica%20do%20LM'%2C%20mas%20gostaria%20de%20fazer%20uma%20pergunta" className="bg-[#25d366] rounded-[100%] w-[4rem] h-[4rem] pt-[1rem] pl-[1.25rem] bottom-[2.25rem] right-[4.15rem] absolute">
           <Image src={require("@/images/whatsapp.svg")} alt={""} width={25} height={25}/>
         </a>
