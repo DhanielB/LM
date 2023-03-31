61e22d0cbbf40a436f6af94727e0f8ca38cf5ecc
Dhaniel Brandão <82909711+DhanielB@users.noreply.github.com>
2023-03-30 21:28:07 -0300

    1º Versão

---
+++ components/AnimatedText.tsx
@@ -0,0 +1,14 @@
+export default function AnimatedText() {
+    return (
+        <div className="text-[#5102FB] inline-block pl-[0.20rem]">
+            <div className="z-10 inline-block w-[5rem] h-[4.85rem] bg-white absolute"></div>
+            <div className="z-10 inline-block mt-[5.95rem] w-[5rem] h-[4.85rem] bg-white absolute"></div>
+            <div className="animate-move">
+                <p className="1">ao IME</p>
+                <p className="2">à ESA</p>
+                <p className="3">à ESPCEX</p>
+                <p className="4">à ITA</p>
+            </div>
+        </div>
+    )
+}
---
+++ components/Timer.tsx
@@ -0,0 +1,7 @@
+export default function Timer() {
+    return (
+        <div  className="inline-block ml-[1rem] bg-[#5102FB] text-white">
+            <p>24 horas</p>
+        </div>
+    )
+}
