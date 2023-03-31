64c6ef54a81426564a7bcf91652bee28794547aa
Dhaniel Brandão <82909711+DhanielB@users.noreply.github.com>
2023-03-31 13:56:42 -0300

    4º versão

--- components/AnimatedText.tsx
+++ components/AnimatedText.tsx
@@ -1,9 +1,9 @@
 export default function AnimatedText() {
     return (
         <div className="text-[#5102FB] inline-block pl-[0.20rem]">
-            <div className="z-10 inline-block w-[5rem] h-[4.85rem] bg-white absolute"></div>
-            <div className="z-10 inline-block mt-[5.95rem] w-[5rem] h-[4.85rem] bg-white absolute"></div>
-            <div className="animate-move">
+            <div className="z-10 inline-block w-[5rem] h-[4.75rem] md:h-[4.85rem] bg-white absolute"></div>
+            <div className="z-10 inline-block mt-[5.95rem] w-[5rem] h-[6.2rem] md:h-[5.85rem] bg-white absolute"></div>
+            <div className="animate-move text-[1.05rem] md:text-base">
                 <p className="1">ao IME</p>
                 <p className="2">à ESA</p>
                 <p className="3">à ESPCEX</p>
--- pages/index.tsx
+++ pages/index.tsx
@@ -14,11 +14,11 @@ export default function Home() {
       <main className="main">
         <h1 className="main-title text-[#201F20] font-extrabold text-[3rem] mt-[5rem] ml-[1.5rem]">MATEMÁTICA<br/> É PARA <p className="inline-block bg-gradient-to-r from-purple-600 to-blue-600 text-transparent bg-clip-text">VOCÊ</p></h1>
         <p className="ml-[1.5rem] text-[1.05rem] inline-block">Trace seu plano de estudos para aprovação rumo</p><AnimatedText/>
-        <div className="left-[30rem] top-[14rem] absolute">
+        <div className="right-[-22.5rem] top-[6rem] md:top-[14rem] md:right-[0.5rem] absolute">
           <Image src={require("@/images/main.gif")} alt={""} width={500} height={500}/>
         </div><br/>
         <button className="bg-[#5102FB] text-white px-[0.5rem] py-[0.35rem] ml-[1.5rem] mt-[4rem] rounded-sm">Quero mandar bem em Matemática em 2023</button>
-        <a href="https://api.whatsapp.com/send?phone=5531995406293&text=Ol%C3%A1%2C%20estou%20no%20site%20da%20'replica%20do%20LM'%2C%20mas%20gostaria%20de%20fazer%20uma%20pergunta" className="bg-[#25d366] rounded-[100%] w-[4rem] h-[4rem] pt-[1rem] pl-[1.25rem] bottom-[2.25rem] right-[4.15rem] absolute">
+        <a href="https://api.whatsapp.com/send?phone=5531995406293&text=Ol%C3%A1%2C%20estou%20no%20site%20da%20'replica%20do%20LM'%2C%20mas%20gostaria%20de%20fazer%20uma%20pergunta" className="z-10 bg-[#25d366] rounded-[100%] w-[4rem] h-[4rem] pt-[1rem] pl-[1.25rem] bottom-[2.25rem] right-[4.15rem] absolute">
           <Image src={require("@/images/whatsapp.svg")} alt={""} width={25} height={25}/>
         </a>
       </main>
